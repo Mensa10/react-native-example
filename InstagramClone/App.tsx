a2e43f0b68001232d93e4f5e8cff6e8ac44779d8
Mensa10 <mensurbektic11@gmail.com>
2019-03-12 19:30:54 +0100

    Added icons.

--- InstagramClone/App.tsx
+++ InstagramClone/App.tsx
@@ -1,13 +1,46 @@
+import * as React from 'react';
 import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import LoginComponent from './src/components/login/LoginComponent';
 import RegisterComponent from './src/components/register/RegisterComponent';
 import FeedComponent from './src/components/feed/FeedComponent';
 
 const bottomNavigator = createBottomTabNavigator({
-  Feed: FeedComponent,
-  Feed2: FeedComponent,
-  Feed3: FeedComponent,
+  Feed: {
+    screen: FeedComponent,
+    navigationOptions: {
+      tabBarLabel: 'Feed',
+      tabBarIcon: ({tintColor} : any) => (
+        <Icon name="ios-home" color={tintColor} size={24} />
+      )
+    }
+  },
+  Feed2: {
+    screen: FeedComponent,
+    navigationOptions: {
+      tabBarLabel: 'Upload',
+      tabBarIcon: ({tintColor} : any) => (
+        <Icon name="ios-cloud-upload" color={tintColor} size={24} />
+      )
+    }
+  },
+  Feed3: {
+    screen: FeedComponent,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({tintColor} : any) => (
+        <Icon name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  },
+}, {
+  initialRouteName: 'Feed',
+  order: ['Feed2', 'Feed', 'Feed3'],
+  tabBarOptions: {
+    activeTintColor: '#FE697C',
+    inactiveTintColor: '#ccc',
+  }
 })
 
 const appStackNavigator = createStackNavigator({
